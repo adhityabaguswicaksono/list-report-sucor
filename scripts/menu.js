90fed1ae1b42e981bdb577c15339978f25125d65
adhityabaguswicaksono <48616245+adhityabaguswicaksono@users.noreply.github.com>
2023-04-10 12:09:29 +0700

    Update Animation Open and Close Sidebar and Profile Menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,3 +1,35 @@
+function functionDropdownProfile() {
+  if (
+    !document
+      .getElementById('dropdown-user')
+      .classList.contains('translate-x-full')
+  ) {
+    document
+      .getElementById('dropdown-user')
+      .classList.add('translate-x-full', 'right-0');
+    document
+      .getElementById('dropdown-user')
+      .classList.remove('translate-x-1', 'right-6');
+  } else {
+    document
+      .getElementById('dropdown-user')
+      .classList.add('translate-x-1', 'right-6');
+    document
+      .getElementById('dropdown-user')
+      .classList.remove('translate-x-full', 'right-0');
+  }
+}
+
+function functionDropdownNavbar() {
+  document.getElementById('sidebar').classList.add('-translate-x-0');
+  document.getElementById('sidebar').classList.remove('-translate-x-full');
+}
+
+function functionCloseDropdownNavbar() {
+  document.getElementById('sidebar').classList.add('-translate-x-full');
+  document.getElementById('sidebar').classList.remove('-translate-x-0');
+}
+
 document.addEventListener(
   'click',
   function (event) {
@@ -5,35 +37,20 @@ document.addEventListener(
       event.target.matches('#dropdown-user') ||
       !event.target.closest('#dropdown-profile')
     ) {
-      document.getElementById('dropdown-user').classList.add('hidden');
       document
         .getElementById('dropdown-user')
-        .classList.remove('absolute', 'top-14', 'right-4');
+        .classList.add('translate-x-full', 'right-0');
+      document
+        .getElementById('dropdown-user')
+        .classList.remove('translate-x-1', 'right-6');
+    }
+    if (
+      event.target.matches('#sidebar') ||
+      !event.target.closest('#toogle-sidebar')
+    ) {
+      document.getElementById('sidebar').classList.add('-translate-x-full');
+      document.getElementById('sidebar').classList.remove('-translate-x-0');
     }
   },
   false
 );
-
-function functionDropdownProfile() {
-  if (document.getElementById('dropdown-user').classList.contains('hidden')) {
-    document.getElementById('dropdown-user').classList.remove('hidden');
-    document
-      .getElementById('dropdown-user')
-      .classList.add('absolute', 'top-14', 'right-4', 'z-50');
-  } else {
-    document.getElementById('dropdown-user').classList.add('hidden');
-    document
-      .getElementById('dropdown-user')
-      .classList.remove('absolute', 'top-14', 'right-4');
-  }
-}
-
-function functionDropdownNavbar() {
-  document.getElementById('sidebar').classList.remove('hidden');
-  document.getElementById('sidebar').classList.add('w-full');
-}
-
-function functionCloseDropdownNavbar() {
-  document.getElementById('sidebar').classList.add('hidden');
-  document.getElementById('sidebar').classList.remove('w-full');
-}
